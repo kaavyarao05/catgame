22bba70e833d9064271db33b23251462c2845c78
kaavya <kaavyarao05@gmail.com>
2024-01-06 18:51:12 +0530

    checks if name is in database, else adds name to db

--- sqlconn.py
+++ sqlconn.py
@@ -1,10 +1,29 @@
 import sqlite3
 
+conn=sqlite3.connect('leaderboard.db')
+cursor=conn.cursor()
+cursor.execute("CREATE TABLE IF NOT EXISTS leaderboard (username VARCHAR(10),score INT);")
+cursor.execute("DELETE FROM leaderboard;")
+conn.commit()
+conn.close()
+
+
 def update(name,points):
     conn=sqlite3.connect('leaderboard.db')
     cursor=conn.cursor()
-    cursor.execute('''INSERT INTO leaderboard (username,score) VALUES ('{}',{});'''.format(name,points))
     conn.commit()
     cursor.execute('''SELECT * FROM leaderboard;''')
     rows=cursor.fetchall()
+    for i in rows:
+        if i[0]==name:
+            cursor.execute("UPDATE leaderboard SET score={} WHERE username='{}'".format(points,name))
+            break
+    else:
+        cursor.execute('''INSERT INTO leaderboard (username,score) VALUES ('{}',{});'''.format(name,points))
+    conn.commit()
+    
+    cursor.execute('''SELECT * FROM leaderboard;''')
+    Rows=cursor.fetchall()
+    for i in Rows:
+        print(i)
     conn.close()
